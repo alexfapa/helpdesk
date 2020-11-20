5b08d8731f4ad8d542f8128797c711c4599ea8e0
Alex Pinto <35609992+alexfapa@users.noreply.github.com>
2020-11-20 13:33:20 -0300

    Adição

--- src/test/java/com/fapa/helpdesk/HelpDeskApplicationTests.java
+++ src/test/java/com/fapa/helpdesk/HelpDeskApplicationTests.java
@@ -1,13 +1,15 @@
 package com.fapa.helpdesk;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class HelpDeskApplicationTests {
+public class HelpDeskApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
