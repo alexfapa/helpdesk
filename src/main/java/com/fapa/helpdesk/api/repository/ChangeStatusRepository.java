3f258cd85c488f97fa6893e2cc1da22ffd15b7bc
Alex Pinto <35609992+alexfapa@users.noreply.github.com>
2020-11-24 17:40:48 -0300

    recolocando os arquivos que haviam sido apagados do diretório repository

--- src/main/java/com/fapa/helpdesk/api/repository/ChangeStatusRepository.java
+++ src/main/java/com/fapa/helpdesk/api/repository/ChangeStatusRepository.java
@@ -1,9 +1,11 @@
 package com.fapa.helpdesk.api.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.fapa.helpdesk.api.entity.ChangeStatus;
 
 
 public interface ChangeStatusRepository extends MongoRepository<ChangeStatus, String>{
 	Iterable<ChangeStatus> findByTicketIdOrderByDateChangeStatus(String ticketId);
 }
+
