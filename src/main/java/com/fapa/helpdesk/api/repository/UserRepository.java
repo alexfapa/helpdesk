920afc0e0eabd7d2edf6717a6c9d2de8a0f437f7
Alex Pinto <35609992+alexfapa@users.noreply.github.com>
2020-11-25 14:56:42 -0300

    Alteração do pom.xml e repository

--- src/main/java/com/fapa/helpdesk/api/repository/UserRepository.java
+++ src/main/java/com/fapa/helpdesk/api/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.fapa.helpdesk.api.repository;
 
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.fapa.helpdesk.api.entity.User;
+
 
 public interface UserRepository extends MongoRepository<User, String>{
 	User findByEmail(String email);
